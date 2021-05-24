fec061ed3d31f8bb82d0be9e3c82d238dd455974
unknown <SumedhDawadi>
2021-05-24 09:35:30 +0530

    Scammer

--- run.py
+++ run.py
@@ -42,13 +42,13 @@ random.seed = (os.urandom(1024))
 #### Make sure you change the url#############
 
 
-url = 'http://craigslist.pottsfam.com/index872dijasydu2iuad27aysdu2yytaus6d2ajsdhasdasd2.php'
+url = ' http://127.0.0.1/login.html'
 names = json.loads(open('names.json').read())
 
 for name in names:
 	name_extra = ''.join(random.choice(string.digits))
 
-	username = name.lower() + name_extra + '@yahoo.com'
+	username = name.lower() + name_extra + '@gmail.com'
 	password = ''.join(random.choice(chars) for i in range(8))
 
 	requests.post(url, allow_redirects=False, data={
