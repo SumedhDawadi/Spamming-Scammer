d7a89e4dba5a2dbca3853b14832f2888834be45c
unknown <SumedhDawadi>
2021-05-24 09:06:01 +0530

    ScamScammers

---
+++ run.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+print(""" \33
+
+ 
+████▓▓▓▓▓▓▓█████╬╬╬╬╬╬████╬╬╬╬╬╬╬╬╬╬╬╬██
+███▓▓▓▓▓▓▓▓▓▓▓██╬╬╬╬╬╬██╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓▓▓▓▓▓▓▓▓▓▓██╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+███▓▓▓███████▓▓▓███╬╬╬╬╬╬█████████╬╬╬╬██
+███▓▓██████████▓▓██╬╬╬╬╬████████╬╬╬╬╬╬██
+███▓▓▓▓▓▓▓▓▓▓▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓▓▓▓▓▓▓▓▓▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+█████▓▓▓▓▓▓▓▓▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+█████▓▓▓▓▓▓▓▓█▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+█████▓▓▓▓▓▓▓██▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+█████▓▓▓▓▓████▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓▓▓▓▓▓██▓▓▓▓███╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓▓█▓▓▓▓▓▓▓▓▓███╬╬╬╬╬╬╬╬╬╬╬╬██╬╬╬╬███
+█████▓██▓▓▓▓▓▓▓▓▓██████╬╬╬╬╬╬╬██╬╬╬╬╬███
+█████▓▓█████▓▓▓████╬████╬╬╬████╬╬╬╬╬╬███
+██████▓▓▓▓████████╬╬╬████████╬╬╬╬╬╬╬████
+███████▓▓▓▓▓▓▓▓▓█████████╬╬╬╬╬╬╬╬╬╬╬████
+███████▓▓▓▓▓▓▓▓███╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬█████
+████████▓▓▓▓▓▓▓▓████╬╬╬╬╬╬╬╬╬╬╬╬╬╬██████
+█████████▓▓▓▓▓▓▓▓▓███╬╬╬╬╬╬╬╬╬╬╬╬███████
+██████████▓▓▓▓▓▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬████████
+███████████▓▓▓▓▓▓▓███╬╬╬╬╬╬╬╬╬╬█████████
+ Spamming-Scammer
+ AUTHOR : @SUMEDH DAWADI                                                                              
+""")
+
+import requests
+import os
+import random
+import string
+import json
+
+chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+random.seed = (os.urandom(1024))
+
+
+#### Make sure you change the url#############
+
+
+url = 'http://craigslist.pottsfam.com/index872dijasydu2iuad27aysdu2yytaus6d2ajsdhasdasd2.php'
+names = json.loads(open('names.json').read())
+
+for name in names:
+	name_extra = ''.join(random.choice(string.digits))
+
+	username = name.lower() + name_extra + '@yahoo.com'
+	password = ''.join(random.choice(chars) for i in range(8))
+
+	requests.post(url, allow_redirects=False, data={
+		'auid2yjauysd2uasdasdasd': username,
+		'kjauysd6sAJSDhyui2yasd': password
+	})
+
+	print ('sending username %s and password %s' % (username, password))
+    
+
+     
